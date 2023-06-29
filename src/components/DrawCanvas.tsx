2ee6f43138058ee221f2bf07eb89f1a280224dd9
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2023-06-29 09:07:20 -0300

    style: added semicolon

--- src/components/DrawCanvas.tsx
+++ src/components/DrawCanvas.tsx
@@ -27,46 +27,46 @@ export class DrawCanvas extends React.Component<Props> {
   componentDidMount(): void {
     this._canvas = document.getElementById(this.props.idCanvas) as HTMLCanvasElement;
     this._context = this._canvas?.getContext('2d') as CanvasRenderingContext2D;
-    this._canvas?.addEventListener("mousedown", this.mouseDown)
-    this._canvas?.addEventListener("mouseup", this.mouseUp)
+    this._canvas?.addEventListener("mousedown", this.mouseDown);
+    this._canvas?.addEventListener("mouseup", this.mouseUp);
   }
 
   draw(point: Point): void {
-    if(!this._context) return
-    this._context.beginPath()
-    this._context.moveTo(this._initialPoint.x, this._initialPoint.y)
-    this._context.lineWidth = this.props.lineWidth || 5
-    this._context.strokeStyle = this.props.strokeStyle || "#000"
-    this._context.lineCap = "round"
-    this._context.lineJoin = "round"
-    this._context.lineTo(point.x, point.y)
-    this._context.stroke()
-    this._initialPoint = point
+    if(!this._context) return;
+    this._context.beginPath();
+    this._context.moveTo(this._initialPoint.x, this._initialPoint.y);
+    this._context.lineWidth = this.props.lineWidth || 5;
+    this._context.strokeStyle = this.props.strokeStyle || "#000";
+    this._context.lineCap = "round";
+    this._context.lineJoin = "round";
+    this._context.lineTo(point.x, point.y);
+    this._context.stroke();
+    this._initialPoint = point;
   }
 
   clear(): void {
     if(!this._context) return
-    this._context.clearRect(0, 0, this.props.width, this.props.height)
+    this._context.clearRect(0, 0, this.props.width, this.props.height);
   }
 
   mouseMoving = (e: any) => {
     this.draw({
       x: e.offsetX,
       y: e.offsetY
-    })
+    });
   }
 
   mouseDown = (e: any) => {
     this._initialPoint = {
       x: e.offsetX,
       y: e.offsetY
-    }
-    this.draw(this._initialPoint)
-    this._canvas?.addEventListener("mousemove", this.mouseMoving)
+    };
+    this.draw(this._initialPoint);
+    this._canvas?.addEventListener("mousemove", this.mouseMoving);
   }
 
   mouseUp = () => {
-    this._canvas?.removeEventListener("mousemove", this.mouseMoving)
+    this._canvas?.removeEventListener("mousemove", this.mouseMoving);
   }
 
   render(): JSX.Element {
